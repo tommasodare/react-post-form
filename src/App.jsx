37b2c9358438936d21a7265bf0452e5c0e97b8e8
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-31 17:32:25 +0200

    Add form handling for author, title, body, and public checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import { useState } from 'react'
 
 export default function App() {
 
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false
+
+  })
+
+  function handleFormData(e) {
+    setFormData((formData) => ({
+      ...formData,
+      [e.target.name]: e.target.value
+    }))
+  }
+
   return (
 
     <>
@@ -12,22 +27,42 @@ export default function App() {
 
           <div>
             <label htmlFor="">Author</label>
-            <input type="text" name="" id="" />
+            <input
+              type="text"
+              name="author"
+              value={formData.author}
+              onChange={handleFormData}
+            />
           </div>
 
           <div>
             <label htmlFor="">Title</label>
-            <input type="text" name="" id="" />
+            <input
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleFormData}
+            />
           </div>
 
           <div>
             <label htmlFor="">Body</label>
-            <input type="text" name="" id="" />
+            <input
+              type="text"
+              name="body"
+              value={formData.body}
+              onChange={handleFormData}
+            />
           </div>
 
           <div>
             <label htmlFor="">Checkbox</label>
-            <input type="checkbox" name="" id="" />
+            <input
+              type="checkbox"
+              name="public"
+              value={formData.public}
+              onChange={handleFormData}
+            />
           </div>
 
         </form>
