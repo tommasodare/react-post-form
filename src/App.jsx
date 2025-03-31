563e03de21696a113c0f19301d49d72105ea37ca
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-31 17:24:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+export default function App() {
+
+  return (
+
+    <>
+
+      <div className="container mt-4">
+
+        <form class="row g-3">
+
+          <div>
+            <label htmlFor="">Author</label>
+            <input type="text" name="" id="" />
+          </div>
+
+          <div>
+            <label htmlFor="">Title</label>
+            <input type="text" name="" id="" />
+          </div>
+
+          <div>
+            <label htmlFor="">Body</label>
+            <input type="text" name="" id="" />
+          </div>
+
+          <div>
+            <label htmlFor="">Checkbox</label>
+            <input type="checkbox" name="" id="" />
+          </div>
+
+        </form>
+
+      </div>
+
+
+
+    </>
+
+
+  )
+
+}
